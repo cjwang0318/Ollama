acd8529ad4252402afadceee14974494d4a29289
cjwang <cjwang0318@gmail.com>
2024-03-19 09:28:13 +0800

    add breeze function

--- langchain_query.py
+++ langchain_query.py
@@ -16,7 +16,8 @@ def convert_tw2s(str):
 
 def Taiwan_llama(query):
     Taiwan_llama_query = f"A chat between a curious user and an artificial intelligence assistant. The assistant gives helpful, detailed, and polite answers to the user's questions. USER: {query} ASSISTANT:"
-    ollama = Ollama(base_url="http://localhost:11434", model="Taiwan-LLaMa:13b-v1")
+    #ollama = Ollama(base_url="http://localhost:11434", model="Taiwan-LLaMa:7b-v2-chat")
+    ollama = Ollama(base_url="http://192.168.50.26:11434", model="Taiwan-LLaMa:7b-v2-chat")
     result = ollama(Taiwan_llama_query)
     print("Taiwan_llama:")
     print(result)
@@ -41,10 +42,22 @@ def mistral(query):
     print(result)
 
 
+def breeze(query):
+    mistral_query = "用中文寫1個 " + query
+    mistral_query = convert_tw2s(mistral_query)
+    ollama = Ollama(
+        base_url="http://192.168.50.26:11434", model="ycchen/breeze-7b-instruct-v1_0", temperature=0.8, top_k=30, repeat_penalty=1.5
+    )
+    result = convert_s2tw(ollama(mistral_query))
+    print("mistral:")
+    print(result)
+
+
 if __name__ == "__main__":
-    query = "連身洋裝的銷售文案"
-    # Taiwan_llama(query)
+    query = "西裝褲的標題，銷售對像是年輕人，有耐穿好穿搭的特色"
+    #Taiwan_llama(query)
+    breeze(query)
     # llama2_chinese(query)
-    mistral(query)
+    # mistral(query)
 
     # https://github.com/langchain-ai/langchain/blob/master/libs/langchain/langchain/llms/ollama.py
